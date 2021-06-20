ce5e654f07a3ec9d6bf93f8cea77a7cc9654eaa2
jpaquim <joaopaquim@gmail.com>
2021-06-20 21:55:56 +0100

    Set up basic manifest.json and service-worker
    Use service-worker.js from svelte-kit test example
    
    Prerender index as static page with no client-side JavaScript

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
+<script context="module">
+  export const router = false;
+  export const hydrate = false;
+  export const prerender = true;
+</script>
+
+<svelte:head>
+  <title>Title</title>
+  <meta name="author" content="pluvial" />
+  <meta name="description" content="pluvial.xyz" />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
