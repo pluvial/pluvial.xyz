5d8e6a035083997e1f0eb465da55faf96f27ef11
jpaquim <joaopaquim@gmail.com>
2021-11-22 19:42:23 +0000

    Remove unnecessary template files and routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,16 +13,7 @@
 </svelte:head>
 
 <section>
-  <h1>
-    <div class="welcome">
-      <picture>
-        <source srcset="svelte-welcome.webp" type="image/webp" />
-        <img src="svelte-welcome.png" alt="Welcome" />
-      </picture>
-    </div>
-
-    to your new<br />SvelteKit app
-  </h1>
+  <h1>Welcome to your new SvelteKit app</h1>
 
   <h2>
     try editing <strong>src/routes/index.svelte</strong>
@@ -42,20 +33,5 @@
 
   h1 {
     width: 100%;
-  }
-
-  .welcome {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
   }
 </style>
