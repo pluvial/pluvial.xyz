f98d00dbd8b44cf291534533a71963a33891f5d7
jpaquim <joaopaquim@gmail.com>
2021-12-11 12:40:24 +0000

    Enable client-side routing in all routes to support transitions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,7 @@
 <script context="module">
+  // import { browser, dev } from '$app/env';
+  // export const hydrate = dev;
+  // export const router = browser;
   export const prerender = true;
 </script>
 
