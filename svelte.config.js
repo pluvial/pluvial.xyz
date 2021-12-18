6abd2a2c987e1114c51bacdc8bc757962b6f7f9a
jpaquim <joaopaquim@gmail.com>
2021-12-18 14:39:14 +0000

    Add mdsvex via svelte-add
    pnpx svelte-add@latest mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,13 @@ import cloudflareWorkers from '@sveltejs/adapter-cloudflare-workers';
 import netlify from '@sveltejs/adapter-netlify';
 import adapter from '@sveltejs/adapter-static';
 import vercel from '@sveltejs/adapter-vercel';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+  preprocess: [mdsvex(mdsvexConfig)],
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
