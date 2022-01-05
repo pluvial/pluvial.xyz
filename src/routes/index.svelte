26a653deb8059c094d408fb35c91bc4cd458b77e
jpaquim <joaopaquim@gmail.com>
2022-01-05 00:31:34 +0000

    List post links in index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,20 @@
   // export const hydrate = dev;
   // export const router = browser;
   export const prerender = true;
+
+  /** @type {import('@sveltejs/kit').Load} */
+  export async function load({ fetch }) {
+    const response = await fetch('/posts.json');
+    const posts = await response.json();
+    return { props: { posts: posts.list } };
+  }
+</script>
+
+<script>
+  /**
+   * @type {{ metadata: { title: string, description: string }, slug: string }[]}
+   */
+  export let posts;
 </script>
 
 <svelte:head>
@@ -13,6 +27,14 @@
 
 <section>
   <h1>pluvial</h1>
+
+  <ul>
+    {#each posts as post (post.slug)}
+      <li>
+        <a sveltekit:prefetch href="/{post.slug}">{post.metadata.title}</a>
+      </li>
+    {/each}
+  </ul>
 
   <h2>
     <a href="https://github.com/pluvial">GitHub</a>
@@ -34,5 +56,15 @@
 
   h1 {
     width: 100%;
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+    min-width: 20em;
+  }
+
+  li {
+    flex: auto;
   }
 </style>
