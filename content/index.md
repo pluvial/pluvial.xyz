5410e62e3a5d5d06010ed7676be23fdf3dfd01f5
jpaquim <joaopaquim@gmail.com>
2022-04-02 13:10:00 +0100

    Use ...catchall route and move index to .md

---
+++ content/index.md
@@ -0,0 +1,10 @@
+---
+title: Home
+---
+
+# pluvial
+
+## Source
+
+- [pluvial/pluvial.xyz](https://github.com/pluvial/pluvial.xyz)
+- [pluvial](https://github.com/pluvial)
--- src/routes/[...slug].svelte
+++ src/routes/[...slug].svelte
@@ -1,6 +1,4 @@
 <script context="module">
-  export const prerender = true;
-
   const prefix = '/content/';
   const suffix = '.md';
   // vite does not support variables in glob imports, but the glob should match
@@ -10,9 +8,9 @@
   const resolvedPosts = {};
 
   /** @type {import('@sveltejs/kit').Load} */
-  export async function load({ params, stuff }) {
+  export async function load({ params, stuff, url }) {
     const { ids, posts, backlinks } = stuff;
-    const { slug } = params;
+    const slug = params.slug || 'index';
     // build the filename from the folder prefix and extension suffix
     const file = `${prefix}${slug}${suffix}`;
     if (file in importsPosts) {
@@ -31,7 +29,7 @@
             content: posts[ids[link]].metadata.title,
           })) ?? [],
       };
-      return { props: { component: module.default, metadata } };
+      return { props: { component: module.default, metadata, path: url.pathname, stuff } };
     }
     // TODO: render fallback content here, use a placeholder page for known links,
     // and a regular page not found otherwise
@@ -48,13 +46,26 @@
    * @property {string} title
    * @property {string} author
    * @property {string} description
-   * @property {{ href: string, content: string }} links
-   * @property {{ href: string, content: string }} externalLinks
-   * @property {{ href: string, content: string }} backlinks
+   * @property {{ href: string, content: string }[]} links
+   * @property {{ href: string, content: string }[]} externalLinks
+   * @property {{ href: string, content: string }[]} backlinks
    */
 
   /** @type {Metadata} */
   export let metadata;
+
+  /** @type {string} */
+  export let path;
+
+  /**
+   * @typedef Stuff
+   * @property {{ metadata: { title: string, description: string }, slug: string }[]} posts
+   * @property {{ [slug: string]: string[] }} links
+   * @property {{ [slug: string]: string[] }} backlinks
+   * 
+
+  /** @type {Stuff} */
+  export let stuff;
 
   const defaults = { title: 'pluvial.xyz', author: 'pluvial', description: 'pluvial.xyz' };
   const { title, author, description, links, externalLinks, backlinks } = {
@@ -69,7 +80,41 @@
   <meta name="description" content={description} />
 </svelte:head>
 
-<svelte:component this={component} />
+{#if path === '/'}
+  <section>
+    <svelte:component this={component} />
+
+    <!-- render page index -->
+    <h2>Pages</h2>
+    <ul>
+      {#each stuff.posts as post (post.slug)}
+        <li>
+          <a sveltekit:prefetch href="/{post.slug}">{post.metadata.title}</a>
+        </li>
+      {/each}
+    </ul>
+
+    <pre><code>{JSON.stringify(stuff, null, 2)}</code></pre>
+  </section>
+
+  <style>
+    section {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      flex: 1;
+      margin: var(--column-margin-top) auto 0 auto;
+    }
+
+    h1,
+    pre {
+      width: 100%;
+    }
+  </style>
+{:else}
+  <svelte:component this={component} />
+{/if}
 
 <footer>
   {#if links.length}
