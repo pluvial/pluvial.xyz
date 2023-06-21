86b0b53e8feff1884cf93661fc3e99ee0851e380
jpaquim <joaopaquim@gmail.com>
2023-06-21 19:18:05 +0100

    Do not show external links in footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,15 +2,15 @@
   /** @type {Link[]} */
   export let links;
   /** @type {Link[]} */
-  export let externalLinks;
+  // export let externalLinks;
   /** @type {Link[]} */
   export let backlinks;
 
   /** @param links {Link[]} */
-  function dedupe(links) {
-    const set = new Set();
-    return links.filter(({ href }) => (set.has(href) ? false : (set.add(href), true)));
-  }
+  // function dedupe(links) {
+  //   const set = new Set();
+  //   return links.filter(({ href }) => (set.has(href) ? false : (set.add(href), true)));
+  // }
 </script>
 
 <footer>
@@ -25,7 +25,7 @@
     </ul>
   {/if}
 
-  {#if externalLinks.length}
+  <!-- {#if externalLinks.length}
     <p>External Links:</p>
     <ul>
       {#each dedupe(externalLinks) as link (link.href)}
@@ -34,7 +34,7 @@
         </li>
       {/each}
     </ul>
-  {/if}
+  {/if} -->
 
   {#if backlinks.length}
     <p>Backlinks:</p>
